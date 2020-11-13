37a8fda6eb4d7a973ffd20a8b8078005190e4278
Augusto Exenberger Becker <augustoexenberger@gmail.com>
2020-11-13 17:24:08 -0300

    Pequenas correções pós-merge

--- src/GeradorDeMusicas/GeradorDeMusica.java
+++ src/GeradorDeMusicas/GeradorDeMusica.java
@@ -7,15 +7,9 @@ package GeradorDeMusicas;
 
 
 
-/**
- *
- * @author Augusto
- */
 public class GeradorDeMusica 
 { 
-    /**
-     * @param args the command line arguments
-     */
+
     public static void main(String[] args) 
     {
         telaPrincipal t = new telaPrincipal();
